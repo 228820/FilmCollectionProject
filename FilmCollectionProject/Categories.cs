cc846c18b3e02c69369244936939a2cd1681c5d3
Piotr Orczykowski <228820@edu.p.lodz.pl>
2023-02-11 14:06:04 +0100

    Add implementation of crud operation in categories form

--- FilmCollectionProject/Categories.cs
+++ FilmCollectionProject/Categories.cs
@@ -64,11 +64,11 @@ namespace FilmCollectionProject
             }
         }
 
-        private bool IsCategoryNameValid()
+        private bool IsCategoryNameToRemoveValid()
         {
-            if ((string)this.selectedCategory.SelectedItem == "")
+            if ((string)this.selectedCategory2.SelectedItem == "" || this.selectedCategory2.SelectedIndex == -1)
             {
-                MessageBox.Show("Please enter a name.");
+                MessageBox.Show("Please enter a name.", "Info");
                 return false;
             }
             else
@@ -77,21 +77,141 @@ namespace FilmCollectionProject
             }
         }
 
+        private bool IsCategoryNameToAddValid()
+        {
+            if (this.categoryName.Text == "")
+            {
+                MessageBox.Show("Please enter a name.", "Info");
+                return false;
+            }
+            else
+            {
+                String categoryName = char.ToUpper(this.categoryName.Text[0]) + this.categoryName.Text?.Substring(1);
+                if (this.selectedCategory.Items.IndexOf(categoryName) != -1)
+                {
+                    MessageBox.Show("Please enter a different name. This one already exists in db.", "Info");
+                    return false;
+                }
+                else
+                {
+                    return true;
+
+                }
+            }
+        }
+
+        private bool IsCategoryNameToEditValid() {
+            if ((string)this.selectedCategory.SelectedItem == "" || this.selectedCategory.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please enter a name.", "Info");
+                return false;
+            } else
+            {
+                if (this.categoryName2.Text == "")
+                {
+                    MessageBox.Show("Please enter a name.", "Info");
+                    return false;
+                }
+                else
+                {
+                    String categoryName = char.ToUpper(this.categoryName2.Text[0]) + this.categoryName2.Text?.Substring(1);
+                    if (this.selectedCategory.Items.IndexOf(categoryName) != -1)
+                    {
+                        MessageBox.Show("Please enter a different name. This one already exists in db.", "Info");
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+
+                    }
+                }
+            }
+        }
+
         private void addCategoryBtn_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Add btn click");
+            String categoryName = this.categoryName.Text;
+            if (this.IsCategoryNameToAddValid())
+            {
+                using (SqlConnection connection = new SqlConnection(this.connectionString))
+                {
+                    try
+                    {
+                        Console.WriteLine("\nQuery data example:");
+                        Console.WriteLine("=========================================\n");
+
+                        connection.Open();
+
+                        String sql = "INSERT INTO category(name) VALUES(@name)";
+                        using (SqlCommand command = new SqlCommand(sql, connection))
+                        {
+                            command.Parameters.Add(new SqlParameter("@name", SqlDbType.VarChar));
+                            command.Parameters["@name"].Value = categoryName;
+                            command.ExecuteReader();
+                            this.categoryName.Text = "";
+                        }
+
+                    }
+                    catch (SqlException ee)
+                    {
+                        MessageBox.Show(ee.Message, "Error Message");
+                    }
+                    finally
+                    {
+                        // Close the connection.
+                        connection.Close();
+                    }
+                }
+                this.refreshData();
+            }
         }
 
         private void editCategoryBtn_Click(object sender, EventArgs e)
         {
+            String categoryName = this.categoryName2.Text;
+            String selectedCategory = (String)this.selectedCategory.SelectedItem;
+            if (this.IsCategoryNameToEditValid())
+            {
+                using (SqlConnection connection = new SqlConnection(this.connectionString))
+                {
+                    try
+                    {
+                        Console.WriteLine("\nQuery data example:");
+                        Console.WriteLine("=========================================\n");
 
+                        connection.Open();
+
+                        String sql = "UPDATE category SET name = @name WHERE name = @selectedName";
+                        using (SqlCommand command = new SqlCommand(sql, connection))
+                        {
+                            command.Parameters.Add(new SqlParameter("@name", SqlDbType.VarChar));
+                            command.Parameters["@name"].Value = categoryName;
+                            command.Parameters.Add(new SqlParameter("@selectedName", SqlDbType.VarChar));
+                            command.Parameters["@selectedName"].Value = selectedCategory;
+                            command.ExecuteReader();
+                            this.categoryName2.Text = "";
+                        }
+
+                    }
+                    catch (SqlException ee)
+                    {
+                        MessageBox.Show(ee.Message, "Error Message");
+                    }
+                    finally
+                    {
+                        // Close the connection.
+                        connection.Close();
+                    }
+                }
+                this.refreshData();
+            }
         }
 
         private void removeCategoryBtn_Click(object sender, EventArgs e)
         {
             String selectedCategory = (String)this.selectedCategory2.SelectedItem;
-
-            if (this.IsCategoryNameValid())
+            if (this.IsCategoryNameToRemoveValid())
             {
                 using (SqlConnection connection = new SqlConnection(this.connectionString))
                 {
@@ -108,6 +228,7 @@ namespace FilmCollectionProject
                             command.Parameters.Add(new SqlParameter("@name", SqlDbType.VarChar));
                             command.Parameters["@name"].Value = selectedCategory;
                             command.ExecuteReader();
+                            this.selectedCategory2.SelectedIndex = -1;
                         }
 
                     }
